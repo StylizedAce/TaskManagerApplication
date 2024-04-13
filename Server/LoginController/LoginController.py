5c0d0a433021974029cdcb98cf10bb8b41879a1a
Yusuf Al-Khafaji <GLBcraft1234@gmail.com>
2024-04-13 23:15:33 +0200

    Logincontrollernot properly creates a user and possible tasks

--- Server/LoginController/LoginController.py
+++ Server/LoginController/LoginController.py
@@ -1,23 +1,28 @@
 from flask import jsonify
 from flask_cors import cross_origin
 
-
 class LogRegController:
-    @cross_origin(origins='*')  # Apply CORS to this route handler
+    
     def login(data, db):
         user = db.users.find_one({'username': data['username'], 'password': data['password']})
         if user:
             return jsonify({'message': 'Login successful'})
         else:
             return jsonify({'message': 'Login failed'})
-        
 
-
-    @cross_origin()  # Apply CORS to this route handler
     def register(data, db):
         user = db.users.find_one({'username': data['username']})
         if user:
             return jsonify({'message': 'User already exists'})
         else:
+            # Insert user into the users collection
             db.users.insert_one(data)
+            
+            # Create an initial entry in the tasks collection for the new user
+            initial_task_entry = {
+                'username': data['username'],
+                'tasks': []
+            }
+            db.tasks.insert_one(initial_task_entry)
+            
             return jsonify({'message': 'User registered'})
