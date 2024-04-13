4dd6612bbb1c5dab23ed15d4f5c09457ab77bcaa
Yusuf Al-Khafaji <GLBcraft1234@gmail.com>
2024-04-13 18:07:29 +0200

    Functional main - change to contr later

--- Server/main.py
+++ Server/main.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 from database import get_database
 from LoginController.LoginController import LogRegController
+from Controllers.TaskController import TaskController
 
 app = Flask(__name__)
 
@@ -16,16 +17,31 @@ db = get_database()
 print(db.users.find)
 
 
-@app.route('/login', methods=['POST'])
+@app.route('/api/login', methods=['POST'])
 def login():
     data = request.json
     return LogRegController.login(data, db)
 
-@app.route('/register', methods=['POST'])
+@app.route('/api//register', methods=['POST'])
 def register():
     data = request.json
     return LogRegController.register(data, db)
 
+
+@app.route('/api/get_tasks', methods=['GET'])
+def getTasks():
+    # Retrieve the username from the request parameters
+    username = request.args.get('username')
+    print(username)
+    
+    entries = db.tasks.find({"username": username})
+
+    tasks = []
+    for entry in entries:
+        tasks.extend(entry['tasks'])    
+
+    print(tasks)
+    return jsonify({'tasks': tasks})
 
 
 if __name__ == '__main__':
