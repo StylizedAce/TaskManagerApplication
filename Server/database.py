8774dea9314cc3b39c73b06609ff8e9c189c1450
Yusuf Al-Khafaji <GLBcraft1234@gmail.com>
2024-04-13 23:10:31 +0200

    removed unnecessary parts

--- Server/database.py
+++ Server/database.py
@@ -2,8 +2,6 @@
 from pymongo import MongoClient
 
 def get_database():
-    # Initialize MongoDB connection
     client = MongoClient('mongodb://localhost:27017')
     db = client['TManagerDB']
-    # Then we print everything in the database
     return db
