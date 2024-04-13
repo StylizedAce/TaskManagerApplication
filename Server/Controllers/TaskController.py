29038178fe6c6d5aa0a4f432ca6daecf739127bd
Yusuf Al-Khafaji <GLBcraft1234@gmail.com>
2024-04-13 23:16:01 +0200

    Taskcontroller functionalities in check

--- Server/Controllers/TaskController.py
+++ Server/Controllers/TaskController.py
@@ -1,24 +1,19 @@
-
-
 from flask import jsonify
 from flask_cors import cross_origin
-
+from bson.objectid import ObjectId
 
 class TaskController:
     
-    @cross_origin(origins='*')  # Apply CORS to this route handler
+    
     def getTasks(username, db):
-        entries = db.tasks.find({"username": "ace"})
-        print(entries)
-        
+        entries = db.tasks.find({"username": username})
         tasks = []
         for entry in entries:
-            tasks.append(entry)
-            
+            tasks.extend(entry['tasks'])    
+        print("here are the tasks", tasks)
         return jsonify({'tasks': tasks})
 
     
-    @cross_origin(origins='*')  # Apply CORS to this route handler
     def addTask(username, task, db):
         user = db.tasks.find_one({"username": username})
         if user:
@@ -28,13 +23,33 @@ class TaskController:
             return jsonify({'error': 'User not found'})
         
     
-    @cross_origin(origins='*')  # Apply CORS to this route handler
     def updateTask(task, db):
         db.tasks.update_one({'_id': task['_id']}, {'$set': task})
         return jsonify({'message': 'Task updated'})
     
     
-    @cross_origin(origins='*')  # Apply CORS to this route handler
-    def deleteTask(task, db):
-        db.tasks.delete_one({'_id': task['_id']})
-        return jsonify({'message': 'Task deleted'})
+    def deleteTask(username, task_id, db):
+        user = db.tasks.find_one({"username": username})    
+        print("Here is the user", user)
+        print ("THere is the task id", task_id)
+        try:
+            db.tasks.update_one({'_id': user['_id']}, {'$pull': {'tasks': {'task_id': int(task_id)}}})
+            return jsonify({'message': 'Task deleted'})
+        except:
+            return jsonify({'error': 'Task not found'})
+
+
+
+    
+    def get_latest_task_id(username, db):
+        latest_task = db.tasks.find_one({"username": username}, sort=[("_id", -1)])
+        if latest_task:
+            tasks = latest_task['tasks']
+            if tasks:
+                # Extract task numbers from task_id fields
+                task_numbers = [task['task_id'] for task in tasks]
+                max_task_number = max(task_numbers)
+                next_task_number = max_task_number + 1
+                next_task_id = next_task_number
+                return jsonify({'next_task_id': next_task_id})
+        return jsonify({'next_task_id': 1})
